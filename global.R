8ed17707cdf15c5ebd3ebbad38d3d770b3500302
Elliot MacKrell <elliot.mackrell@gmail.com>
2023-04-28 15:45:16 -0700

    Add modified heatmap function for Shiny Server compatibility

--- global.R
+++ global.R
@@ -280,3 +280,82 @@ plot_pca_mod <- function (data = NULL, variables = c("PC1", "PC2"), labels = TRU
 }
 
 environment(plot_pca_mod) <- asNamespace("tidyproteomics")
+
+
+# Modified heatmap function to prevent file writing
+# Required for successful plotting on Shiny Server
+plot_heatmap_mod <- function(
+    data = NULL,
+  tag=NULL,
+  row_names=FALSE,
+  ...
+){
+  
+  # visible bindings
+  color <- NULL
+  abundance <- NULL
+  sample_rep <- NULL
+  
+  check_data(data)
+  
+  quantval <- data$quantitative_source
+  data_quant <- data %>% extract(values = quantval, na.rm = TRUE) %>%
+    dplyr::select(!dplyr::matches('^origin$'))
+  theme_palette <- theme_palette()
+  
+  data_col <- data_quant %>%
+    dplyr::group_by(sample) %>%
+    dplyr::summarise(
+      n = replicate %>% unique() %>% length(),
+      .groups = 'drop'
+    )
+  
+  data_col <- data_col %>%
+    dplyr::mutate(color = theme_palette[1:nrow(data_col)])
+  
+  group_color <- list(sample = data_col %>% dplyr::select(color) %>% unlist() %>% as.vector())
+  names(group_color$sample) <- data_col$sample
+  
+  data_munge <- data_quant %>%
+    dplyr::mutate(abundance = log10(abundance)) %>%
+    tidyr::pivot_wider(
+      names_from = c('sample', 'replicate'),
+      values_from = 'abundance',
+      values_fill = 0
+    ) %>% as.data.frame() %>%
+    tibble::column_to_rownames('identifier') %>%
+    as.matrix()
+  
+  col <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(10, "RdYlBu"))(32) %>% rev()
+  
+  plot <- pheatmap::pheatmap(data_munge,
+    scale = "none",
+    silent = TRUE,
+    col =  col,
+    show_rownames = row_names,
+    annotation_colors = group_color,
+    #annotation_legend = FALSE,
+    #legend = FALSE,
+    annotation_names_col = FALSE,
+    annotation_col = data_quant %>%
+      dplyr::select(sample, replicate) %>%
+      unique() %>%
+      tidyr::unite(sample_rep, sample, replicate, remove = F) %>%
+      dplyr::select(!replicate) %>%
+      as.data.frame() %>%
+      tibble::column_to_rownames('sample_rep'),
+    main = glue::glue("Heatmap: {data$analyte} {data$quantitative_source} \n")
+  )
+  
+  pdf(NULL)
+  
+  if(!'null device' %in% names(grDevices::dev.cur())) { invisible(grDevices::dev.off()) }
+  
+  return(plot_save(plot,
+    data,
+    glue::glue("{data$analyte}_{data$quantitative_source}_heatmap"),
+    ...))
+  
+}
+
+environment(plot_heatmap_mod) <- asNamespace("tidyproteomics")
